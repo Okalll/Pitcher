50f985075a304db83ca229006f75ef8ae08d9b67
Okall Vivian <vivieokall@gmail.com>
2019-02-13 09:32:39 +0300

    first commit

---
+++ Pitcher/config.py
@@ -0,0 +1,47 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:sarahjt12@localhost/pitches'
+    SQLALCHEMY_TRACk_NOTIFICATIONS = False
+    SECRET_KEY='Cake124'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    #  email configurations
+    # MAIL_SERVER = 'sjtoek.googlemail.com'
+    # MAIL_PORT = 587
+    # MAIL_USE_TLS = True
+    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+
+    '''
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
